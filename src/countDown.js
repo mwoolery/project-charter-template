9fa501512f07d6fff9e28ba46843605499e8ec0c
mwoolery <mwoolery@nwmissouri.edu>
2018-10-19 13:17:13 -0500

    removed the countdown timer from showing up on index since it is just saying "DaysExpired", it doesn't really provide useful information anymore.

--- src/countDown.js
+++ src/countDown.js
@@ -9,11 +9,11 @@ function countDownfunc() {
           var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
           var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
           var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-          document.getElementById("countDown").innerHTML = days + "  Days   " + hours + "  Hours   "
+          //document.getElementById("countDown").innerHTML = days + "  Days   " + hours + "  Hours   "
             + minutes + "  Minutes   " + seconds + "  Seconds   ";
           if (distance < 0) {
             clearInterval(x);
-          document.getElementById("countDown").innerHTML = "DAYSEXPIRED";
+          //document.getElementById("countDown").innerHTML = "";
           }
         }, 1000);
     }
