738a33fabddd2544619ab40d658658233a1ed756
Devineni,Sai Sirisha <S531367@nwmissouri.edu>
2018-10-28 00:19:14 -0500

    modal date

--- src/modal.js
+++ src/modal.js
@@ -1,5 +1,15 @@
+var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1; 
+var day = dateObj.getUTCDate();
+var year = dateObj.getUTCFullYear();
+if(month==9 && day<=31 && year==2018){
+    $(window).on('load',function(){
+        $('#myModal').modal('show');
+    });
+    document.getElementsByClassName('modalbtn').style.visibility = 'visible';
+}
+else{
+    document.getElementsByClassName('modalbtn').style.visibility = 'hidden';
+}
 
 
-$(window).on('load',function(){
-    $('#myModal').modal('show');
-});
