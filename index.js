7db0ad799efcf76679f32fc69e64935b892940cd
Sairam <satyasairamvankina@bitbucket.org>
2018-10-28 17:24:21 -0500

    Done the contact form and given all entries

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ var express = require('express'),
 const _port = 3000
 
 var app = express();
-
-
+curl -s --user 'api:4836d8f5-93b1e7a3' \
+    https://api.mailgun.net/v3/sandboxa16b182489814caba2256e4c4d40ecda.mailgun.org/messages \
+        -F from='Mailgun Sandbox <postmaster@sandboxa16b182489814caba2256e4c4d40ecda.mailgun.org>' \
+        -F to='sairam <s531508@nwmissouri.edu>' \
+        -F subject='Hello sairam' \
+        -F text='Congratulations sairam, you just sent an email with Mailgun!  You are truly awesome!'
 
 app.use(express.static(path.join(__dirname, '/..', './src')));
 
