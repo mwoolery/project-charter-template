a9b2f217cf3dfb8634aa110d4116f66047da5b7f
mwoolery <mwoolery@nwmissouri.edu>
2018-11-02 11:00:23 -0500

    adding a use statement to get it to route to all the assets since there is a lot of errors coming up since it can't find assets to render on the various pages

--- src/app.js
+++ src/app.js
@@ -8,13 +8,12 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 
-// use views from view folder and use ejs
-//app.use(express.static('public'));
-console.log("__dirname " + __dirname);
-console.log("process.cwd " + process.cwd());
+
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public/'));
 
 
 var route = require('./controllers/route');
