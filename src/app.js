8693f42bb98e4bfbb890fca458b9886611f5a15f
mwoolery <mwoolery@nwmissouri.edu>
2018-11-08 12:50:03 -0600

    put in a sample data item into a data folder which will be read to test locally before we create the actual mongodb

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ var express = require('express'),
 
 var port = process.env.PORT || 3000;
 
+
+//Database set up in code block below
+//const Datastore = require('nedb');
+//var db = new Datastore();
+//db.loadDatabase();
+
+
+
 var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
@@ -13,8 +21,18 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public/'));
 
 
+//set up seed data
+//var bannerItems = require('./data/banneritem.json');
+//db.insert(bannerItems);
+//app.locals.bannerItems = db.find(bannerItems);
+//console.log(Object.keys(bannerItems).length+ " bannerItems");
+//app.use('/banneritem', require('./controllers/banneritem.js'));
+
+
 var route = require('./controllers/route');
 route(app)
+
+
 
 
 app.listen(port)
