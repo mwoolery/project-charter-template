0d1994e8e7ed19b9b7fc02f3b616863159c137dd
mwoolery <mwoolery@nwmissouri.edu>
2018-11-08 12:19:57 -0600

    created the model views and controller files that will be needed for the banner editor portion of the app.  They are blank files at the moment

---
+++ src/models/bannerItem.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+    , Schema = mongoose.Schema
+
+
+//description is the text that displays in the banner
+//startDate will be the datetime that it starts displaying
+//endDate will be the datetime that it ends    
+var BannerItemSchema = new Schema({
+	_id: { type: Number, required: true },
+    description: { type: String, required: true },
+    startDate: { type: Date, required: true},
+    endDate: { type: Date, required: true}
+})
+
+var bannerItem = mongoose.model('BannerItem', BannerItemSchema)
+module.exports = BannerItem
