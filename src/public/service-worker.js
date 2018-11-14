4af56e7d24022de4ee5e9b491425beaefe99e0de
mwoolery <mwoolery@nwmissouri.edu>
2018-11-14 10:40:37 -0600

    trying to get the service worker to register on heroku

---
+++ src/public/service-worker.js
@@ -0,0 +1,61 @@
+const offlinePage = '/offline.ejs';
+
+var dataCacheName = 'HughesFHdataCache';
+var cacheName = 'HughesFHcache';
+var filesToCache = [
+  '/',
+ 
+  
+
+ 
+
+ 
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+
+  return self.clients.claim();
+});
+
+
+// check for the cache
+self.addEventListener('fetch', (event) => {
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      caches.match(event.request)
+      .then((cached) => {
+        var networked = fetch(event.request)
+          .then((response) => {
+            let cacheCopy = response.clone()
+            caches.open(cacheName)
+              .then(cache => cache.put(event.request, cacheCopy))
+            return response;
+          })
+          //.catch(() => caches.match(offlinePage));
+        return cached || networked;
+      })
+    )
+  }
+  return;
+});
