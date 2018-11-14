1a811c8493524c559799301c41447c3ad3854bcb
mwoolery <mwoolery@nwmissouri.edu>
2018-11-14 11:41:15 -0600

    I think I fixed the service-worker.js but I'm not sure if it will add to homescreen

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -1,59 +1,47 @@
-const offlinePage = '/offline.html';
+var CACHE = 'HughesFHcache';
 
-var dataCacheName = 'HughesFHdataCache';
-var cacheName = 'HughesFHcache';
-var filesToCache = [
-  '/',
- 
-  
-
- 
-
- 
-];
+addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(CACHE)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            
+        }
+      })
+  );
+});      
 
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll([
+        '/'
+      ]);
     })
   );
-});
+ });
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
+        if (key !== CACHE) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
     })
   );
-
-  return self.clients.claim();
-});
-
-
-// check for the cache
-self.addEventListener('fetch', (event) => {
-  if (event.request.method != 'GET') return;
-
-  event.respondWith(async function() {
-    
-    const cache = await caches.open(cacheName);
-    const cachedResponse = await cache.match(event.request);
-
-    
-    event.respondWith(
-        fetch(event.request).catch(function() {
-          return caches.match(event.request);
-        }));
-
-    return fetch(event.request);
-  }());
+   return self.clients.claim();
 });
