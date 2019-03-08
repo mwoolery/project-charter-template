342922e4fb7f41ca309d260614e884221fa3afb1
mwoolery <mwoolery@nwmissouri.edu>
2019-03-08 10:13:04 -0600

    modified the service worker a bit, but ended up deciding that we won't be able to use it because it causes problems with the database that can't be gotten around due to the nature of PWAs.

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -1,11 +1,12 @@
 // trying this service worker https://stackoverflow.com/questions/50074992/service-worker-network-first-then-cache-with-fallback-to-static-page modfied a bit for our needs.
-var CACHE_STATIC_NAME = 'hfcache';
-var CACHE_DYNAMIC_NAME = 'hfhcached';
+var STATIC_CACHE = 'hfcache-s';
+var DYNAMIC_CACHE = 'hfhcache-d';
 
+//install the serviceworker
 self.addEventListener('install', function (event) {
   console.log('Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open(CACHE_STATIC_NAME)
+    caches.open(STATIC_CACHE)
       .then(function (cache) {
         console.log('Precaching App Shell');
         
@@ -13,13 +14,14 @@ self.addEventListener('install', function (event) {
   )
 });
 
+// activate after it is registered
 self.addEventListener('activate', function (event) {
   console.log('Activating Service Worker ....', event);
   event.waitUntil(
     caches.keys()
       .then(function (keyList) {
         return Promise.all(keyList.map(function (key) {
-          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+          if (key !== STATIC_CACHE && key !== DYNAMIC_CACHE) {
             console.log('Removing old cache.', key);
             return caches.delete(key);
           }
@@ -29,21 +31,24 @@ self.addEventListener('activate', function (event) {
   return self.clients.claim();
 });
 
+
+// fetch method that takes over normal fetches and does PWA ones
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    // Try the network
+    // Try networking first
     fetch(event.request)
       .then(function(res) {
-        return caches.open(CACHE_DYNAMIC_NAME)
+        return caches.open(DYNAMIC_CACHE)
           .then(function(cache) {
-            // Put in cache if succeeds
+            // cache succefully networked requests
             cache.put(event.request.url, res.clone());
             return res;
           })
       })
       .catch(function(err) {
-          // Fallback to cache
+          // do cached 
           return caches.match(event.request).then(function(res){
+            // 404
             if (res === undefined) { 
               res.send('error 404');
             } 
